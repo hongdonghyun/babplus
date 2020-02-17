9c993767192700557f771e7e564711f5f8293a1c
hongdonghyun <hong4367@gmail.com>
2020-02-17 21:43:26 +0900

    SettingViewController 수정

--- babPlus/babPlus/Controllers/SettingViewController.swift
+++ babPlus/babPlus/Controllers/SettingViewController.swift
@@ -8,23 +8,53 @@
 
 import UIKit
 
-class SettingViewController: UIViewController {
+class SettingViewController: UITableViewController {
+    private var settings = [Settings]()
+    private lazy var sections = SettingType.caseList.map { $0.rawValue }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        tableView.register(SettingTableViewCell.self, forCellReuseIdentifier: SettingTableViewCell.indetifier)
+        
+        settings = SettingType.caseList.map {
+            Settings(key: $0.rawValue, settingOptions: $0.getInfo())
+        }
+        view.backgroundColor = AssetsColor.babplusBackground.getColor()
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
     }
     
+}
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+extension SettingViewController {
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return sections.count
     }
-    */
 
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return settings.count
+    }
+
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sections[section]
+
+    }
+    
+    override func tableView(_ tableView: UITableView, willDisplayHeaderView view: UIView, forSection section: Int) {
+        if let headerView = view as? UITableViewHeaderFooterView {
+            headerView.contentView.backgroundColor = AssetsColor.babplusBackground.getColor()
+            headerView.textLabel?.textColor = AssetsColor.babplusTableCellTextColor.getColor()
+            headerView.textLabel?.textAlignment = .left
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "settingCell", for: indexPath)as? SettingTableViewCell
+            else { return UITableViewCell() }
+        
+        let setting = settings[indexPath.section].settingOptions[indexPath.row]
+        cell.configure(settingOptions: setting)
+        return cell
+    }
 }
