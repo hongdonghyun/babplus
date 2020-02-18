94c286d7c88b588c9e6134b6ba707f1c1d8e8174
hongdonghyun <hong4367@gmail.com>
2020-02-18 21:59:04 +0900

    세팅 상세 커스텀뷰 추가

--- babPlus/babPlus/Views/SettingViews/SettingUIView.swift
+++ babPlus/babPlus/Views/SettingViews/SettingUIView.swift
@@ -9,13 +9,44 @@
 import UIKit
 
 class SettingUIView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    let menuTextField: UITextField = {
+        let textField = UITextField()
+        textField.font = .systemFont(ofSize: 25, weight: .medium)
+        return textField
+    }()
+    
+    let underLine: UIView = {
+        let view = UIView()
+        view.backgroundColor = AssetsColor.babplusUnderLineColor.getColor()
+        return view
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
     }
-    */
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupUI() {
+        let safeArea = self.safeAreaLayoutGuide
+        
+        [menuTextField, underLine].forEach {
+            self.addSubview($0)
+            $0.translatesAutoresizingMaskIntoConstraints = false
+        }
+        
+        NSLayoutConstraint.activate([
+            menuTextField.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 40),
+            menuTextField.centerXAnchor.constraint(equalTo: safeArea.centerXAnchor),
+            menuTextField.heightAnchor.constraint(equalToConstant: 40),
+            
+            underLine.topAnchor.constraint(equalTo: menuTextField.bottomAnchor, constant: 5),
+            underLine.centerXAnchor.constraint(equalTo: menuTextField.centerXAnchor),
+            underLine.widthAnchor.constraint(equalTo: menuTextField.widthAnchor, multiplier: 1),
+            underLine.heightAnchor.constraint(equalToConstant: 1)
+        ])
+    }
 }
